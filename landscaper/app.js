9792a4d5078bc7f4397ec47ddc23e5592d4ff263
mmergal92 <mmergal92@gmail.com>
2021-03-14 19:36:00 -0400

    landscaper: user can buy push lawnmower

--- landscaper/app.js
+++ landscaper/app.js
@@ -2,3 +2,4 @@
 //using your teeth, can make $1
 //At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money.
 //Using the rusty scissors, you can spend the day cutting lawns and make $5. You can do this as much as you want.
+//At any point, if you are currently using rusty scissors, you can buy an old-timey push lawnmower for $25. You can do this once, assuming you have enough money.

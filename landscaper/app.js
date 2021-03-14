79517701bf1b0838460636139dd1eaa614721b18
mmergal92 <mmergal92@gmail.com>
2021-03-14 19:36:33 -0400

    landscaper: user can use push lawnmower to cut grass

--- landscaper/app.js
+++ landscaper/app.js
@@ -3,3 +3,4 @@
 //At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money.
 //Using the rusty scissors, you can spend the day cutting lawns and make $5. You can do this as much as you want.
 //At any point, if you are currently using rusty scissors, you can buy an old-timey push lawnmower for $25. You can do this once, assuming you have enough money.
+//Using the old-timey push lawnmower, you can spend the day cutting lawns and make $50. You can do this as much as you want.

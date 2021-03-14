03d922a30ee3b947c43ec52bca230df0374337f0
mmergal92 <mmergal92@gmail.com>
2021-03-14 19:32:32 -0400

    landscaper: user can buy scissors

--- landscaper/app.js
+++ landscaper/app.js
@@ -1,1 +1,3 @@
 //landscaping business, all you have are teeth
+//using your teeth, can make $1
+//At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money.

0edfe0b814166d8637ed9076d984b38b919e7a19
mmergal92 <mmergal92@gmail.com>
2021-03-14 19:34:09 -0400

    landscaper app: user can use scissors to cut grass

--- landscaper/app.js
+++ landscaper/app.js
@@ -1,3 +1,4 @@
 //landscaping business, all you have are teeth
 //using your teeth, can make $1
 //At any point, if you are currently using your teeth, you can buy a pair of rusty scissors for $5. You can do this once, assuming you have enough money.
+//Using the rusty scissors, you can spend the day cutting lawns and make $5. You can do this as much as you want.

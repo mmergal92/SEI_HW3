0bd0136f2820c41755ad9d17c39b8bb7a00838f9
mmergal92 <mmergal92@gmail.com>
2021-03-14 18:54:25 -0400

    created person class

---
+++ landscaper/app.js
@@ -0,0 +1,7 @@
+class Landscaper{
+ this.tool = teeth;
+ this.pay = 1
+}
+
+const todayLandscaper = new Landscaper
+console.log (todayLandscaper)
--- objects.js
+++ objects.js
@@ -13,7 +13,7 @@ class Hamster{
         console.log('nibble nibble')
     }
     getPrice(){
-        console.log(this.price)
+        return this.price
     }
 }
 const hamster1 = new Hamster ("steph", "rover")
@@ -21,3 +21,46 @@ console.log (hamster1)
 hamster1.wheelRun()
 hamster1.eatFood()
 hamster1.getPrice()
+
+//Person
+class Person{
+    constructor (name, age, height, weight, mood, hamsters, bankAccount){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters =[];
+        this.bankAccount = 0;
+    }
+    getName(){
+        console.log(this.name)
+    }
+    getAge(){
+        console.log(this.age)
+    }
+    getWeight(){
+        console.log(this.weight)
+    }
+    greet(){
+      console.log("hi! How are you " + this.name + " ?")
+    }
+    eat(){
+        this.weight++
+        this.mood++ 
+    }
+    exercise(){
+        this.weight--
+    }
+    ageUp(){
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount = this.bankAccount + 10
+    }
+    buyHamster(){
+        this.hamsters.push(hamster1)
+        this.bankAccount = this.bankAccount - hamster1.getPrice()
+    }
+}

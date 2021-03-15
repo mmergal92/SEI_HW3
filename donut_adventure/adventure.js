826e003b9d1528a9896ff66bf19cdf5491e88b12
mmergal92 <mmergal92@gmail.com>
2021-03-15 01:51:37 -0400

    created our enemy pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -21,3 +21,30 @@ class Hero{
 }
 const Donut = new Hero ("Dougie")
 console.log (Donut)
+
+class Enemy{
+    constructor (name, health, weapons, catchPhrases){
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        }
+        this.catchPhrases = ['i\'m Youtube famous', 'i\'m more dangerous than an uncovered sewer']
+    }
+    talkSass(){
+        function randomNumInRange (min, max){
+            let n = Math.random()*(max-min)+min;
+            return(n);
+          }
+        console.log()
+    }
+    announcHealth(){
+        console.log('Your current health levels are at ' + this.health)
+    }
+    fight(){
+        console.log('I\'m gonna flatten you like a slice of pepperoni!')
+    }
+}
+const pizzaRat = new Enemy ("Pizza Rat")
+console.log (pizzaRat)
